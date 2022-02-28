c140114c73307728080790ebd95ae6f17c6ae4d1
JG Carvalho <jg@jgcarv.com>
2022-02-28 20:01:56 -0300

    fix rebasing issues

---
+++ contracts/test/mocks/MockERC20.sol
@@ -0,0 +1,36 @@
+
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.7;
+
+import { ERC20 } from "../../ERC20.sol";
+
+import { ERC20Permit } from "../../ERC20Permit.sol";
+
+contract MockERC20 is ERC20 {
+
+    constructor(string memory name_, string memory symbol_, uint8 decimals_) ERC20(name_, symbol_, decimals_) {}
+
+    function mint(address to_, uint256 value_) external {
+        _mint(to_, value_);
+    }
+
+    function burn(address from_, uint256 value_) external {
+        _burn(from_, value_);
+    }
+
+}
+
+contract MockERC20Permit is ERC20Permit {
+
+    constructor(string memory name_, string memory symbol_, uint8 decimals_) ERC20Permit(name_, symbol_, decimals_) {}
+
+    function mint(address to_, uint256 value_) external {
+        _mint(to_, value_);
+    }
+
+    function burn(address from_, uint256 value_) external {
+        _burn(from_, value_);
+    }
+
+}
+
