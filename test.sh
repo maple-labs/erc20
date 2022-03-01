e79bde0b21d3b1907d0d653fd6309c6345f6ccac
Lucas Manuel <lucas@maple.finance>
2022-03-01 13:57:40 -0500

    fix: update test.sh

--- test.sh
+++ test.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-while getopts t:r:b:v:c: flag
+while getopts t:p: flag
 do
     case "${flag}" in
         t) test=${OPTARG};;
-        r) runs=${OPTARG};;
+        r) profile=${OPTARG};;
     esac
 done
 
-runs=$([ -z "$runs" ] && echo "10" || echo "$runs")
-
-export DAPP_SOLC_VERSION=0.8.7
-export PROPTEST_CASES=$runs
+export FOUNDRY_PROFILE=$profile
 
 if [ -z "$test" ]; then match="[contracts/test/*.t.sol]"; else match=$test; fi
 
