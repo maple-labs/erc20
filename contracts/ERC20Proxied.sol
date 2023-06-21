c64a24f51434cc568958c50b29dec5fddc95c168
Vedran Bidin <vedran@bidin.me>
2023-06-21 16:44:24 +0300

    fix: replace functions with storage

--- contracts/ERC20Proxied.sol
+++ contracts/ERC20Proxied.sol
@@ -22,6 +22,11 @@ abstract contract ERC20Proxied is IERC20 {
     /*** ERC-20                                                                                                                         ***/
     /**************************************************************************************************************************************/
 
+    string public override name;
+    string public override symbol;
+
+    uint8 public override decimals;
+
     uint256 public override totalSupply;
 
     mapping(address => uint256) public override balanceOf;
@@ -36,17 +41,6 @@ abstract contract ERC20Proxied is IERC20 {
     bytes32 public constant override PERMIT_TYPEHASH = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
 
     mapping(address => uint256) public override nonces;
-
-
-    /**************************************************************************************************************************************/
-    /*** Overriding Functions                                                                                                           ***/
-    /**************************************************************************************************************************************/
-
-    function decimals() public view virtual override returns (uint8 decimals_);
-
-    function name() public view virtual override returns (string memory name_);
-
-    function symbol() public view virtual override returns (string memory symbol_);
 
     /**************************************************************************************************************************************/
     /*** External Functions                                                                                                             ***/
@@ -117,7 +111,7 @@ abstract contract ERC20Proxied is IERC20 {
         return keccak256(
             abi.encode(
                 keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
-                keccak256(bytes(name())),
+                keccak256(bytes(name)),
                 keccak256(bytes("1")),
                 block.chainid,
                 address(this)
